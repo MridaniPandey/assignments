122c1681f93beb84375de9e18a397ff094e104e1
Mridani Pandey <mridanipandey8813@gmail.com>
2024-11-19 18:41:30 +0545

    added pragram.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swappingelements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            // Swap without temp variable using addition and subtraction
+            a = a + b; // a becomes 15
+            b = a - b; // b becomes 5 (original value of a)
+            a = a - b; // a becomes 10 (original value of b)
+
+            Console.WriteLine($"a: {a}, b: {b}");
+
+        }
+    }
+}
